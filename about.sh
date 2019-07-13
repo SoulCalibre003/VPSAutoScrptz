89ec8e4ed119e69617ddfc2a9620bd2f808e0ac3
Soul Calibre <52732022+SoulCalibre003@users.noreply.github.com>
2019-07-13 12:51:45 +0000

    Add files via upload

---
+++ about.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#About this script
+
+clear
+echo -e "Script Auto Install SSH & OpenVPN v2.0" | lolcat 
+echo -e ""
+echo -e "For Debian 8"
+echo -e "For VPS with KVM and VMWare virtualization"
+echo -e ""
+echo -e "Original script by :"
+echo -e "* Fornesia"
+echo -e "* Rzengineer"
+echo -e "* Fawzya"
+echo -e ""
+echo -e "Modified by SoulCalibre"
+echo -e ""
+echo -e "https://www.facebook.com/iknowiamsocute"
+echo -e "https://www.facebook.com/SoulCalibre007"
+echo -e "https://www.youtube.com/JonathanBayudang"
+echo -e ""
